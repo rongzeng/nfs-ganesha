14f60e4c5858cb6ca2c46533f419f0c97b24d76d
rong zeng <rongzeng@us.ibm.com>
2012-08-22 13:20:54 -0700

    1. Fix in atomic_store_xxx_t: pass value to __sync_lock_test_and_set. This was causing    nsm_client not able to turn on monitor flag. 2. Change nsm timeout from 5 to 25 seconds.
    Port from 1.5.x:
    5ada096bd2f67613c0b485c2b24aa198f0f23150
    
    Signed-off-by: rong zeng <rongzeng@us.ibm.com>

--- src/include/abstract_atomic.h
+++ src/include/abstract_atomic.h
@@ -2029,7 +2029,7 @@ atomic_store_size_t(size_t *var, size_t val)
 static inline void
 atomic_store_size_t(size_t *var, size_t val)
 {
-     __sync_lock_test_and_set(var, 0);
+     __sync_lock_test_and_set(var, val);
 }
 #endif
 
@@ -2078,7 +2078,7 @@ atomic_store_ptrdiff_t(ptrdiff_t *var, ptrdiff_t val)
 static inline void
 atomic_store_ptrdiff_t(ptrdiff_t *var, ptrdiff_t val)
 {
-     __sync_lock_test_and_set(var, 0);
+     __sync_lock_test_and_set(var, val);
 }
 #endif
 
@@ -2127,7 +2127,7 @@ atomic_store_int64_t(int64_t *var, int64_t val)
 static inline void
 atomic_store_int64_t(int64_t *var, int64_t val)
 {
-     __sync_lock_test_and_set(var, 0);
+     __sync_lock_test_and_set(var, val);
 }
 #endif
 
@@ -2176,7 +2176,7 @@ atomic_store_uint64_t(uint64_t *var, uint64_t val)
 static inline void
 atomic_store_uint64_t(uint64_t *var, uint64_t val)
 {
-     __sync_lock_test_and_set(var, 0);
+     __sync_lock_test_and_set(var, val);
 }
 #endif
 
@@ -2225,7 +2225,7 @@ atomic_store_int32_t(int32_t *var, int32_t val)
 static inline void
 atomic_store_int32_t(int32_t *var, int32_t val)
 {
-     __sync_lock_test_and_set(var, 0);
+     __sync_lock_test_and_set(var, val);
 }
 #endif
 
@@ -2274,7 +2274,7 @@ atomic_store_uint32_t(uint32_t *var, uint32_t val)
 static inline void
 atomic_store_uint32_t(uint32_t *var, uint32_t val)
 {
-     __sync_lock_test_and_set(var, 0);
+     __sync_lock_test_and_set(var, val);
 }
 #endif
 
@@ -2323,7 +2323,7 @@ atomic_store_int16_t(int16_t *var, int16_t val)
 static inline void
 atomic_store_int16_t(int16_t *var, int16_t val)
 {
-     __sync_lock_test_and_set(var, 0);
+     __sync_lock_test_and_set(var, val);
 }
 #endif
 
@@ -2372,7 +2372,7 @@ atomic_store_uint16_t(uint16_t *var, uint16_t val)
 static inline void
 atomic_store_uint16_t(uint16_t *var, uint16_t val)
 {
-     __sync_lock_test_and_set(var, 0);
+     __sync_lock_test_and_set(var, val);
 }
 #endif
 
@@ -2421,7 +2421,7 @@ atomic_store_int8_t(int8_t *var, int8_t val)
 static inline void
 atomic_store_int8_t(int8_t *var, int8_t val)
 {
-     __sync_lock_test_and_set(var, 0);
+     __sync_lock_test_and_set(var, val);
 }
 #endif
 
@@ -2470,7 +2470,7 @@ atomic_store_uint8_t(uint8_t *var, uint8_t val)
 static inline void
 atomic_store_uint8_t(uint8_t *var, uint8_t val)
 {
-     __sync_lock_test_and_set(var, 0);
+     __sync_lock_test_and_set(var, val);
 }
 #endif
 #endif /* !_ABSTRACT_ATOMIC_H */
